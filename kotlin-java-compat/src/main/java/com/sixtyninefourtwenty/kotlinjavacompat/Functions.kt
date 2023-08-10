84e50431cd9f1ad60b861f2877ba317d477089df
bruh <quangtrung02hn16@gmail.com>
2023-08-10 14:05:28 +0700

    [kotlin-java-compat] Add throwing functions

--- kotlin-java-compat/src/main/java/com/sixtyninefourtwenty/kotlinjavacompat/Functions.kt
+++ kotlin-java-compat/src/main/java/com/sixtyninefourtwenty/kotlinjavacompat/Functions.kt
@@ -1,6 +1,10 @@
 @file:Suppress("unused")
 package com.sixtyninefourtwenty.kotlinjavacompat
 
+import com.sixtyninefourtwenty.kotlinjavacompat.Function0Compat.Companion.shorten
+import com.sixtyninefourtwenty.kotlinjavacompat.Function1Compat.Companion.shorten
+import kotlin.jvm.Throws
+
 /**
  * A functional interface that can be used in place of Kotlin's `() -> Unit` to get rid of the awkward `return Unit.INSTANCE` return statement.
  */
@@ -30,7 +34,24 @@ fun interface Function0Compat : () -> Unit {
          */
         @JvmStatic
         fun shorten(block: Function0Compat): () -> Unit = block
+
+        /**
+         * Same as [shorten], but allows the [block] to propagate exceptions.
+         */
+        @JvmStatic
+        fun shortenThrowing(block: ThrowingFunction0Compat): () -> Unit = block
     }
+}
+
+fun interface ThrowingFunction0Compat : Function0Compat {
+
+    override fun invokeCustom() {
+        invokeCustomThrowing()
+    }
+
+    @Throws(Throwable::class)
+    fun invokeCustomThrowing()
+
 }
 
 /**
@@ -62,7 +83,24 @@ fun interface Function1Compat<in T> : Function1<T, Unit> {
          */
         @JvmStatic
         fun <T> shorten(block: Function1Compat<T>): (T) -> Unit = block
+
+        /**
+         * Same as [shorten], but allows the [block] to propagate exceptions.
+         */
+        @JvmStatic
+        fun <T> shortenThrowing(block: ThrowingFunction1Compat<T>): (T) -> Unit = block
     }
+}
+
+fun interface ThrowingFunction1Compat<in T> : Function1Compat<T> {
+
+    override fun invokeCustom(t: T) {
+        invokeCustomThrowing(t)
+    }
+
+    @Throws(Throwable::class)
+    fun invokeCustomThrowing(t: T)
+
 }
 
 /**
@@ -94,5 +132,22 @@ fun interface Function2Compat<in T, in U> : Function2<T, U, Unit> {
          */
         @JvmStatic
         fun <T, U> shorten(block: Function2Compat<T, U>): (T, U) -> Unit = block
+
+        /**
+         * Same as [shorten], but allows the [block] to propagate exceptions.
+         */
+        @JvmStatic
+        fun <T, U> shortenThrowing(block: ThrowingFunction2Compat<T, U>): (T, U) -> Unit = block
     }
+}
+
+fun interface ThrowingFunction2Compat<in T, in U> : Function2Compat<T, U> {
+
+    override fun invokeCustom(t: T, u: U) {
+        invokeCustomThrowing(t, u)
+    }
+
+    @Throws(Throwable::class)
+    fun invokeCustomThrowing(t: T, u: U)
+
 }
