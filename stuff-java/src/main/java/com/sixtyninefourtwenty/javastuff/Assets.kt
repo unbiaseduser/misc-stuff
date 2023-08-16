c5f38dd108e8db729c6dc95cd557ab8b0fd258ad
bruh <quangtrung02hn16@gmail.com>
2023-08-16 20:01:00 +0700

    [stuff-java] Revamp Assets

--- stuff-java/src/main/java/com/sixtyninefourtwenty/javastuff/Assets.kt
+++ stuff-java/src/main/java/com/sixtyninefourtwenty/javastuff/Assets.kt
@@ -5,13 +5,15 @@ package com.sixtyninefourtwenty.javastuff
 
 import android.content.res.AssetManager
 import android.graphics.drawable.Drawable
+import java.nio.charset.Charset
+import java.util.stream.Stream
 
-fun AssetManager.openQuietly(path: String) = open(path)
-
-fun AssetManager.openBufferedReader(path: String) = open(path).bufferedReader()
-
-fun AssetManager.readEntireTextFile(path: String) = openBufferedReader(path).use { it.readText() }
-
-fun AssetManager.openBufferedStream(path: String) = open(path).buffered()
+@JvmOverloads
+fun AssetManager.readEntireTextFile(path: String, charset: Charset = Charsets.UTF_8) =
+    open(path).bufferedReader(charset).use { it.readText() }
 
 fun AssetManager.createDrawableFromAsset(path: String) = open(path).use { Drawable.createFromStream(it, path) }
+
+@JvmOverloads
+fun <R> AssetManager.readTextFileLines(path: String, charset: Charset = Charsets.UTF_8, block: (Stream<String>) -> R) =
+    open(path).bufferedReader(charset).use { block(it.lines()) }
