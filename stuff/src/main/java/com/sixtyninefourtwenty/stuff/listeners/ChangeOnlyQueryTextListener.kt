2d9b2de4cdbc9ba47b09cf624052b9321abe3705
bruh <quangtrung02hn16@gmail.com>
2023-08-22 17:46:00 +0700

    [stuff] Fix

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/listeners/ChangeOnlyQueryTextListener.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/listeners/ChangeOnlyQueryTextListener.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.widget.SearchView
 @Suppress("unused")
 fun interface ChangeOnlyQueryTextListener : SearchView.OnQueryTextListener {
 
-    override fun onQueryTextChange(newText: String): Boolean
+    override fun onQueryTextChange(newText: String): Boolean {
+        onQueryTextChangeCustom(newText)
+        return true
+    }
 
     override fun onQueryTextSubmit(query: String): Boolean {
         return false
     }
+
+    fun onQueryTextChangeCustom(newText: String)
 
     companion object {
         @JvmStatic
