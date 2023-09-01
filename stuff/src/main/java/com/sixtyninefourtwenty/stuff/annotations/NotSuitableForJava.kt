b16de0767a8aad0b9ba3a48225448c8680dfa16a
bruh <quangtrung02hn16@gmail.com>
2023-09-01 12:16:34 +0700

    [stuff] Make annotation have runtime retention

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/annotations/NotSuitableForJava.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/annotations/NotSuitableForJava.kt
@@ -2,5 +2,5 @@ package com.sixtyninefourtwenty.stuff.annotations
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.FILE, AnnotationTarget.CLASS)
 @MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
+@Retention
 internal annotation class NotSuitableForJava(val reason: String)
