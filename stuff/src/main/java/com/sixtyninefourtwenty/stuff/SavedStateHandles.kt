3369c6a886a5f2200dee2bfa3b02d77838f078be
bruh <quangtrung02hn16@gmail.com>
2023-09-01 10:10:32 +0700

    [stuff] Add more savedstatehandle extensions

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/SavedStateHandles.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/SavedStateHandles.kt
@@ -10,76 +10,152 @@ import java.io.Serializable
 fun SavedStateHandle.getBoolean(key: String) = get<Boolean>(key)
 fun SavedStateHandle.getBooleanOrDefault(key: String, defValue: Boolean) = getBoolean(key) ?: defValue
 fun SavedStateHandle.setBoolean(key: String, value: Boolean) = set(key, value)
+inline fun SavedStateHandle.getOrSetBoolean(key: String, block: () -> Boolean) = getBoolean(key) ?: run {
+    block().also { setBoolean(key, it) }
+}
 
 fun SavedStateHandle.getByte(key: String) = get<Byte>(key)
 fun SavedStateHandle.getByteOrDefault(key: String, defValue: Byte) = getByte(key) ?: defValue
 fun SavedStateHandle.setByte(key: String, value: Byte) = set(key, value)
+inline fun SavedStateHandle.getOrSetByte(key: String, block: () -> Byte) = getByte(key) ?: run {
+    block().also { setByte(key, it) }
+}
 
 fun SavedStateHandle.getChar(key: String) = get<Char>(key)
 fun SavedStateHandle.getCharOrDefault(key: String, defValue: Char) = getChar(key) ?: defValue
 fun SavedStateHandle.setChar(key: String, value: Char) = set(key, value)
+inline fun SavedStateHandle.getOrSetChar(key: String, block: () -> Char) = getChar(key) ?: run {
+    block().also { setChar(key, it) }
+}
 
 fun SavedStateHandle.getDouble(key: String) = get<Double>(key)
 fun SavedStateHandle.getDoubleOrDefault(key: String, defValue: Double) = getDouble(key) ?: defValue
 fun SavedStateHandle.setDouble(key: String, value: Double) = set(key, value)
+inline fun SavedStateHandle.getOrSetDouble(key: String, block: () -> Double) = getDouble(key) ?: run {
+    block().also { setDouble(key, it) }
+}
 
 fun SavedStateHandle.getFloat(key: String) = get<Float>(key)
 fun SavedStateHandle.getFloatOrDefault(key: String, defValue: Float) = getFloat(key) ?: defValue
 fun SavedStateHandle.setFloat(key: String, value: Float) = set(key, value)
+inline fun SavedStateHandle.getOrSetFloat(key: String, block: () -> Float) = getFloat(key) ?: kotlin.run {
+    block().also { setFloat(key, it) }
+}
 
 fun SavedStateHandle.getInt(key: String) = get<Int>(key)
 fun SavedStateHandle.getIntOrDefault(key: String, defValue: Int) = getInt(key) ?: defValue
 fun SavedStateHandle.setInt(key: String, value: Int) = set(key, value)
+inline fun SavedStateHandle.getOrSetInt(key: String, block: () -> Int) = getInt(key) ?: run {
+    block().also { setInt(key, it) }
+}
 
 fun SavedStateHandle.getLong(key: String) = get<Long>(key)
 fun SavedStateHandle.getLongOrDefault(key: String, defValue: Long) = getLong(key) ?: defValue
 fun SavedStateHandle.setLong(key: String, value: Long) = set(key, value)
+inline fun SavedStateHandle.getOrSetLong(key: String, block: () -> Long) = getLong(key) ?: run {
+    block().also { setLong(key, it) }
+}
 
 fun SavedStateHandle.getShort(key: String) = get<Short>(key)
 fun SavedStateHandle.getShortOrDefault(key: String, defValue: Short) = getShort(key) ?: defValue
 fun SavedStateHandle.setShort(key: String, value: Short) = set(key, value)
+inline fun SavedStateHandle.getOrSetShort(key: String, block: () -> Short) = getShort(key) ?: run {
+    block().also { setShort(key, it) }
+}
 
 fun SavedStateHandle.getBundle(key: String) = get<Bundle>(key)
 fun SavedStateHandle.setBundle(key: String, value: Bundle) = set(key, value)
+inline fun SavedStateHandle.getOrSetBundle(key: String, block: () -> Bundle) = getBundle(key) ?: run {
+    block().also { setBundle(key, it) }
+}
 
 fun SavedStateHandle.getCharSequence(key: String) = get<CharSequence>(key)
 fun SavedStateHandle.setCharSequence(key: String, value: CharSequence) = set(key, value)
+inline fun SavedStateHandle.getOrSetCharSequence(key: String, block: () -> CharSequence) = getCharSequence(key) ?: run {
+    block().also { setCharSequence(key, it) }
+}
+
+fun SavedStateHandle.getString(key: String) = get<String>(key)
+fun SavedStateHandle.getStringOrDefault(key: String, defValue: String) = getString(key) ?: defValue
+fun SavedStateHandle.setString(key: String, value: String) = set(key, value)
+inline fun SavedStateHandle.getOrSetString(key: String, block: () -> String) = getString(key) ?: run {
+    block().also { setString(key, it) }
+}
 
 fun <T : Parcelable> SavedStateHandle.getParcelable(key: String) = get<T>(key)
 fun SavedStateHandle.setParcelable(key: String, value: Parcelable) = set(key, value)
+inline fun <T : Parcelable> SavedStateHandle.getOrSetParcelable(key: String, block: () -> T) = getParcelable(key) ?: run {
+    block().also { setParcelable(key, it) }
+}
 
 fun SavedStateHandle.getBooleanArray(key: String) = get<BooleanArray>(key)
 fun SavedStateHandle.setBooleanArray(key: String, value: BooleanArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetBooleanArray(key: String, block: () -> BooleanArray) = getBooleanArray(key) ?: run {
+    block().also { setBooleanArray(key, it) }
+}
 
 fun SavedStateHandle.getByteArray(key: String) = get<ByteArray>(key)
 fun SavedStateHandle.setByteArray(key: String, value: ByteArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetByteArray(key: String, block: () -> ByteArray) = getByteArray(key) ?: run {
+    block().also { setByteArray(key, it) }
+}
 
 fun SavedStateHandle.getCharArray(key: String) = get<CharArray>(key)
 fun SavedStateHandle.setCharArray(key: String, value: CharArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetCharArray(key: String, block: () -> CharArray) = getCharArray(key) ?: run {
+    block().also { setCharArray(key, it) }
+}
 
 fun SavedStateHandle.getDoubleArray(key: String) = get<DoubleArray>(key)
 fun SavedStateHandle.setDoubleArray(key: String, value: DoubleArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetDoubleArray(key: String, block: () -> DoubleArray) = getDoubleArray(key) ?: run {
+    block().also { setDoubleArray(key, it) }
+}
 
 fun SavedStateHandle.getFloatArray(key: String) = get<FloatArray>(key)
 fun SavedStateHandle.setFloatArray(key: String, value: FloatArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetFloatArray(key: String, block: () -> FloatArray) = getFloatArray(key) ?: run {
+    block().also { setFloatArray(key, it) }
+}
 
 fun SavedStateHandle.getIntArray(key: String) = get<IntArray>(key)
 fun SavedStateHandle.setIntArray(key: String, value: IntArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetIntArray(key: String, block: () -> IntArray) = getIntArray(key) ?: run {
+    block().also { setIntArray(key, it) }
+}
 
 fun SavedStateHandle.getLongArray(key: String) = get<LongArray>(key)
 fun SavedStateHandle.setLongArray(key: String, value: LongArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetLongArray(key: String, block: () -> LongArray) = getLongArray(key) ?: run {
+    block().also { setLongArray(key, it) }
+}
 
 fun SavedStateHandle.getShortArray(key: String) = get<ShortArray>(key)
 fun SavedStateHandle.setShortArray(key: String, value: ShortArray) = set(key, value)
+inline fun SavedStateHandle.getOrSetShortArray(key: String, block: () -> ShortArray) = getShortArray(key) ?: run {
+    block().also { setShortArray(key, it) }
+}
 
 fun SavedStateHandle.getParcelableArray(key: String) = get<Array<Parcelable>>(key)
 fun SavedStateHandle.setParcelableArray(key: String, value: Array<Parcelable>) = set(key, value)
+inline fun SavedStateHandle.getOrSetParcelableArray(key: String, block: () -> Array<Parcelable>) = getParcelableArray(key) ?: run {
+    block().also { setParcelableArray(key, it) }
+}
 
 fun SavedStateHandle.getStringArray(key: String) = get<Array<String>>(key)
 fun SavedStateHandle.setStringArray(key: String, value: Array<String>) = set(key, value)
+inline fun SavedStateHandle.getOrSetStringArray(key: String, block: () -> Array<String>) = getStringArray(key) ?: run {
+    block().also { setStringArray(key, it) }
+}
 
 fun SavedStateHandle.getCharSequenceArray(key: String) = get<Array<CharSequence>>(key)
 fun SavedStateHandle.setCharSequenceArray(key: String, value: Array<CharSequence>) = set(key, value)
+inline fun SavedStateHandle.getOrSetCharSequenceArray(key: String, block: () -> Array<CharSequence>) = getCharSequenceArray(key) ?: run {
+    block().also { setCharSequenceArray(key, it) }
+}
 
 fun <T : Serializable> SavedStateHandle.getSerializable(key: String) = get<T>(key)
 fun SavedStateHandle.setSerializable(key: String, value: Serializable) = set(key, value)
+inline fun <T : Serializable> SavedStateHandle.getOrSetSerializable(key: String, block: () -> T) = getSerializable(key) ?: run {
+    block().also { setSerializable(key, it) }
+}
--- stuff/src/test/java/com/sixtyninefourtwenty/stuff/SavedStateHandleExtensionsTest.kt
+++ stuff/src/test/java/com/sixtyninefourtwenty/stuff/SavedStateHandleExtensionsTest.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.lifecycle.SavedStateHandle
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNull
@@ -32,187 +33,236 @@ class SavedStateHandleExtensionsTest {
     fun booleanTest() {
         assertNull(savedStateHandle.getBoolean(KEY))
         assertFalse(savedStateHandle.getBooleanOrDefault(KEY, false))
-        savedStateHandle.setBoolean(KEY, true)
-        assertTrue(savedStateHandle.getBooleanOrDefault(KEY, false))
+        assertTrue(savedStateHandle.getOrSetBoolean(KEY) { true })
+        assertTrue(savedStateHandle.getBoolean(KEY)!!)
+        savedStateHandle.setBoolean(KEY, false)
+        assertFalse(savedStateHandle.getBoolean(KEY)!!)
     }
 
     @Test
     fun byteTest() {
         assertNull(savedStateHandle.getByte(KEY))
         assertEquals(-1, savedStateHandle.getByteOrDefault(KEY, -1))
+        assertEquals(2, savedStateHandle.getOrSetByte(KEY) { 2 })
+        assertEquals(2, savedStateHandle.getByte(KEY))
         savedStateHandle.setByte(KEY, 1)
-        assertEquals(1, savedStateHandle.getByteOrDefault(KEY, -1))
+        assertEquals(1, savedStateHandle.getByte(KEY))
     }
 
     @Test
     fun charTest() {
         assertNull(savedStateHandle.getChar(KEY))
         assertEquals('a', savedStateHandle.getCharOrDefault(KEY, 'a'))
+        assertEquals('c', savedStateHandle.getOrSetChar(KEY) { 'c' })
+        assertEquals('c', savedStateHandle.getChar(KEY))
         savedStateHandle.setChar(KEY, 'b')
-        assertEquals('b', savedStateHandle.getCharOrDefault(KEY, 'a'))
+        assertEquals('b', savedStateHandle.getChar(KEY))
     }
 
     @Test
     fun doubleTest() {
         assertNull(savedStateHandle.getDouble(KEY))
         assertEquals(Double.MIN_VALUE, savedStateHandle.getDoubleOrDefault(KEY, Double.MIN_VALUE))
+        assertEquals(2.1, savedStateHandle.getOrSetDouble(KEY) { 2.1 })
+        assertEquals(2.1, savedStateHandle.getDouble(KEY))
         savedStateHandle.setDouble(KEY, 1.0)
-        assertEquals(1.0, savedStateHandle.getDoubleOrDefault(KEY, Double.MIN_VALUE))
+        assertEquals(1.0, savedStateHandle.getDouble(KEY))
     }
 
     @Test
     fun floatTest() {
         assertNull(savedStateHandle.getFloat(KEY))
         assertEquals(Float.MIN_VALUE, savedStateHandle.getFloatOrDefault(KEY, Float.MIN_VALUE))
+        assertEquals(3.1F, savedStateHandle.getOrSetFloat(KEY) { 3.1F })
+        assertEquals(3.1F, savedStateHandle.getFloat(KEY))
         savedStateHandle.setFloat(KEY, 1.0F)
-        assertEquals(1.0F, savedStateHandle.getFloatOrDefault(KEY, Float.MIN_VALUE))
+        assertEquals(1.0F, savedStateHandle.getFloat(KEY))
     }
 
     @Test
     fun intTest() {
         assertNull(savedStateHandle.getInt(KEY))
         assertEquals(Int.MIN_VALUE, savedStateHandle.getIntOrDefault(KEY, Int.MIN_VALUE))
+        assertEquals(2, savedStateHandle.getOrSetInt(KEY) { 2 })
+        assertEquals(2, savedStateHandle.getInt(KEY))
         savedStateHandle.setInt(KEY, 1)
-        assertEquals(1, savedStateHandle.getIntOrDefault(KEY, Int.MIN_VALUE))
+        assertEquals(1, savedStateHandle.getInt(KEY))
     }
 
     @Test
     fun longTest() {
         assertNull(savedStateHandle.getLong(KEY))
         assertEquals(Long.MIN_VALUE, savedStateHandle.getLongOrDefault(KEY, Long.MIN_VALUE))
+        assertEquals(3, savedStateHandle.getOrSetLong(KEY) { 3 })
+        assertEquals(3, savedStateHandle.getLong(KEY))
         savedStateHandle.setLong(KEY, 1)
-        assertEquals(1, savedStateHandle.getLongOrDefault(KEY, Long.MIN_VALUE))
+        assertEquals(1, savedStateHandle.getLong(KEY))
     }
 
     @Test
     fun shortTest() {
         assertNull(savedStateHandle.getShort(KEY))
         assertEquals(Short.MIN_VALUE, savedStateHandle.getShortOrDefault(KEY, Short.MIN_VALUE))
+        assertEquals(4, savedStateHandle.getOrSetShort(KEY) { 4 })
+        assertEquals(4, savedStateHandle.getShort(KEY))
         savedStateHandle.setShort(KEY, 1)
-        assertEquals(1, savedStateHandle.getShortOrDefault(KEY, Short.MIN_VALUE))
+        assertEquals(1, savedStateHandle.getShort(KEY))
     }
 
     @Test
     fun bundleTest() {
         assertNull(savedStateHandle.getBundle(KEY))
-        val bundle = Bundle()
-        savedStateHandle.setBundle(KEY, bundle)
-        assertEquals(bundle, savedStateHandle.getBundle(KEY))
+        val b1 = Bundle()
+        assertEquals(b1, savedStateHandle.getOrSetBundle(KEY) { b1 })
+        assertEquals(b1, savedStateHandle.getBundle(KEY))
+        val b2 = Bundle()
+        savedStateHandle.setBundle(KEY, b2)
+        assertEquals(b2, savedStateHandle.getBundle(KEY))
     }
 
     @Test
     fun charSequenceTest() {
         assertNull(savedStateHandle.getCharSequence(KEY))
-        val stringBuilder = StringBuilder()
-        savedStateHandle.setCharSequence(KEY, stringBuilder)
-        assertEquals(stringBuilder, savedStateHandle.getCharSequence(KEY))
+        val sb1 = StringBuilder("foo")
+        assertEquals(sb1, savedStateHandle.getOrSetCharSequence(KEY) { sb1 })
+        assertEquals(sb1, savedStateHandle.getCharSequence(KEY))
+        val sb2 = StringBuilder()
+        savedStateHandle.setCharSequence(KEY, sb2)
+        assertEquals(sb2, savedStateHandle.getCharSequence(KEY))
+    }
+
+    @Test
+    fun stringTest() {
+        assertNull(savedStateHandle.getString(KEY))
+        assertEquals("bar", savedStateHandle.getStringOrDefault(KEY, "bar"))
+        assertEquals("foo", savedStateHandle.getOrSetString(KEY) { "foo" })
+        assertEquals("foo", savedStateHandle.getString(KEY))
+        savedStateHandle.setString(KEY, "e")
+        assertEquals("e", savedStateHandle.getString(KEY))
     }
 
     @Test
     fun parcelableTest() {
         assertNull(savedStateHandle.getParcelable(KEY))
-        val obj = MyParcelable()
-        savedStateHandle.setParcelable(KEY, obj)
-        assertEquals(obj, savedStateHandle.getParcelable(KEY))
+        val obj1 = MyParcelable()
+        assertEquals(obj1, savedStateHandle.getOrSetParcelable(KEY) { obj1 })
+        assertEquals(obj1, savedStateHandle.getParcelable(KEY))
+        val obj2 = MyParcelable()
+        savedStateHandle.setParcelable(KEY, obj2)
+        assertEquals(obj2, savedStateHandle.getParcelable(KEY))
     }
 
     @Test
     fun serializableTest() {
         assertNull(savedStateHandle.getSerializable(KEY))
-        val today = LocalDate.now()
-        savedStateHandle.setSerializable(KEY, today)
-        assertEquals(today, savedStateHandle.getSerializable(KEY))
+        assertEquals(LocalDate.now().plusDays(1), savedStateHandle.getOrSetSerializable(KEY) { LocalDate.now().plusDays(1) })
+        assertEquals(LocalDate.now().plusDays(1), savedStateHandle.getSerializable(KEY))
+        savedStateHandle.setSerializable(KEY, LocalDate.now())
+        assertEquals(LocalDate.now(), savedStateHandle.getSerializable(KEY))
     }
 
     @Test
     fun booleanArrayTest() {
         assertNull(savedStateHandle.getBooleanArray(KEY))
-        val array = BooleanArray(1)
-        savedStateHandle.setBooleanArray(KEY, array)
-        assertEquals(array, savedStateHandle.getBooleanArray(KEY))
+        assertArrayEquals(booleanArrayOf(true, false), savedStateHandle.getOrSetBooleanArray(KEY) { booleanArrayOf(true, false) })
+        assertArrayEquals(booleanArrayOf(true, false), savedStateHandle.getBooleanArray(KEY))
+        savedStateHandle.setBooleanArray(KEY, BooleanArray(1))
+        assertArrayEquals(BooleanArray(1), savedStateHandle.getBooleanArray(KEY))
     }
 
     @Test
     fun byteArrayTest() {
         assertNull(savedStateHandle.getByteArray(KEY))
-        val array = ByteArray(1)
-        savedStateHandle.setByteArray(KEY, array)
-        assertEquals(array, savedStateHandle.getByteArray(KEY))
+        assertArrayEquals(byteArrayOf(0, 1), savedStateHandle.getOrSetByteArray(KEY) { byteArrayOf(0, 1) })
+        assertArrayEquals(byteArrayOf(0, 1), savedStateHandle.getByteArray(KEY))
+        savedStateHandle.setByteArray(KEY, ByteArray(1))
+        assertArrayEquals(ByteArray(1), savedStateHandle.getByteArray(KEY))
     }
 
     @Test
     fun charArrayTest() {
         assertNull(savedStateHandle.getCharArray(KEY))
-        val array = CharArray(1)
-        savedStateHandle.setCharArray(KEY, array)
-        assertEquals(array, savedStateHandle.getCharArray(KEY))
+        assertArrayEquals(charArrayOf('a', 'b'), savedStateHandle.getOrSetCharArray(KEY) { charArrayOf('a', 'b') })
+        assertArrayEquals(charArrayOf('a', 'b'), savedStateHandle.getCharArray(KEY))
+        savedStateHandle.setCharArray(KEY, CharArray(1))
+        assertArrayEquals(CharArray(1), savedStateHandle.getCharArray(KEY))
     }
 
     @Test
     fun doubleArrayTest() {
         assertNull(savedStateHandle.getDoubleArray(KEY))
-        val array = DoubleArray(1)
-        savedStateHandle.setDoubleArray(KEY, array)
-        assertEquals(array, savedStateHandle.getDoubleArray(KEY))
+        assertArrayEquals(doubleArrayOf(1.0, 3.1), savedStateHandle.getOrSetDoubleArray(KEY) { doubleArrayOf(1.0, 3.1) })
+        assertArrayEquals(doubleArrayOf(1.0, 3.1), savedStateHandle.getDoubleArray(KEY))
+        savedStateHandle.setDoubleArray(KEY, DoubleArray(1))
+        assertArrayEquals(DoubleArray(1), savedStateHandle.getDoubleArray(KEY))
     }
 
     @Test
     fun floatArrayTest() {
         assertNull(savedStateHandle.getFloatArray(KEY))
-        val array = FloatArray(1)
-        savedStateHandle.setFloatArray(KEY, array)
-        assertEquals(array, savedStateHandle.getFloatArray(KEY))
+        assertArrayEquals(floatArrayOf(1.0F, 3.1F), savedStateHandle.getOrSetFloatArray(KEY) { floatArrayOf(1.0F, 3.1F) })
+        assertArrayEquals(floatArrayOf(1.0F, 3.1F), savedStateHandle.getFloatArray(KEY))
+        savedStateHandle.setFloatArray(KEY, FloatArray(1))
+        assertArrayEquals(FloatArray(1), savedStateHandle.getFloatArray(KEY))
     }
 
     @Test
     fun intArrayTest() {
         assertNull(savedStateHandle.getIntArray(KEY))
-        val array = IntArray(1)
-        savedStateHandle.setIntArray(KEY, array)
-        assertEquals(array, savedStateHandle.getIntArray(KEY))
+        assertArrayEquals(intArrayOf(6, 9), savedStateHandle.getOrSetIntArray(KEY) { intArrayOf(6, 9) })
+        assertArrayEquals(intArrayOf(6, 9), savedStateHandle.getIntArray(KEY))
+        savedStateHandle.setIntArray(KEY, IntArray(1))
+        assertArrayEquals(IntArray(1), savedStateHandle.getIntArray(KEY))
     }
 
     @Test
     fun longArrayTest() {
         assertNull(savedStateHandle.getLongArray(KEY))
-        val array = LongArray(1)
-        savedStateHandle.setLongArray(KEY, array)
-        assertEquals(array, savedStateHandle.getLongArray(KEY))
+        assertArrayEquals(longArrayOf(6, 9), savedStateHandle.getOrSetLongArray(KEY) { longArrayOf(6, 9) })
+        assertArrayEquals(longArrayOf(6, 9), savedStateHandle.getLongArray(KEY))
+        savedStateHandle.setLongArray(KEY, LongArray(1))
+        assertArrayEquals(LongArray(1), savedStateHandle.getLongArray(KEY))
     }
 
     @Test
     fun shortArrayTest() {
         assertNull(savedStateHandle.getShortArray(KEY))
-        val array = ShortArray(1)
-        savedStateHandle.setShortArray(KEY, array)
-        assertEquals(array, savedStateHandle.getShortArray(KEY))
+        assertArrayEquals(shortArrayOf(2, 3), savedStateHandle.getOrSetShortArray(KEY) { shortArrayOf(2, 3) })
+        assertArrayEquals(shortArrayOf(2, 3), savedStateHandle.getShortArray(KEY))
+        savedStateHandle.setShortArray(KEY, ShortArray(1))
+        assertArrayEquals(ShortArray(1), savedStateHandle.getShortArray(KEY))
     }
 
     @Test
     fun parcelableArrayTest() {
         assertNull(savedStateHandle.getParcelableArray(KEY))
-        val array = arrayOf<Parcelable>()
-        savedStateHandle.setParcelableArray(KEY, array)
-        assertEquals(array, savedStateHandle.getParcelableArray(KEY))
+        assertArrayEquals(arrayOf(MyParcelable()), savedStateHandle.getOrSetParcelableArray(KEY) { arrayOf(MyParcelable()) })
+        assertArrayEquals(arrayOf(MyParcelable()), savedStateHandle.getParcelableArray(KEY))
+        savedStateHandle.setParcelableArray(KEY, arrayOf())
+        assertArrayEquals(arrayOf<Parcelable>(), savedStateHandle.getParcelableArray(KEY))
     }
 
     @Test
     fun stringArrayTest() {
         assertNull(savedStateHandle.getStringArray(KEY))
-        val array = arrayOf<String>()
-        savedStateHandle.setStringArray(KEY, array)
-        assertEquals(array, savedStateHandle.getStringArray(KEY))
+        assertArrayEquals(arrayOf("foo"), savedStateHandle.getOrSetStringArray(KEY) { arrayOf("foo") })
+        assertArrayEquals(arrayOf("foo"), savedStateHandle.getStringArray(KEY))
+        savedStateHandle.setStringArray(KEY, arrayOf())
+        assertArrayEquals(arrayOf(), savedStateHandle.getStringArray(KEY))
     }
 
     @Test
     fun charSequenceArrayTest() {
         assertNull(savedStateHandle.getCharSequenceArray(KEY))
-        val array = arrayOf<CharSequence>()
-        savedStateHandle.setCharSequenceArray(KEY, array)
-        assertEquals(array, savedStateHandle.getCharSequenceArray(KEY))
+        val arr1 = arrayOf<CharSequence>(StringBuilder("foo"), StringBuffer("bar"))
+        assertEquals(arr1, savedStateHandle.getOrSetCharSequenceArray(KEY) { arr1 })
+        assertEquals(arr1, savedStateHandle.getCharSequenceArray(KEY))
+        val arr2 = arrayOf<CharSequence>()
+        savedStateHandle.setCharSequenceArray(KEY, arr2)
+        assertEquals(arr2, savedStateHandle.getCharSequenceArray(KEY))
     }
 
-    private class MyParcelable : Parcelable {
+    private data class MyParcelable(val something: String = "something") : Parcelable {
         override fun describeContents(): Int = 0
 
         override fun writeToParcel(dest: Parcel, flags: Int) {
