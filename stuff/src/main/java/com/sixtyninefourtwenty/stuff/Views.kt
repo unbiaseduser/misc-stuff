b82c6f092d2327a32edfa4f78f9ab21035e0bfb4
bruh <quangtrung02hn16@gmail.com>
2023-09-05 11:26:16 +0700

    [stuff] Add convenience textwatcher stuff

--- stuff/src/main/java/com/sixtyninefourtwenty/stuff/Views.kt
+++ stuff/src/main/java/com/sixtyninefourtwenty/stuff/Views.kt
@@ -7,11 +7,29 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import com.google.android.material.textfield.TextInputLayout
 import com.sixtyninefourtwenty.stuff.annotations.NotSuitableForJava
+import com.sixtyninefourtwenty.stuff.listeners.TextWatcherAdapter
 
 fun EditText.isBlank() = text == null || text.toString().isBlank()
 
 fun EditText.getInput() = text?.toString()?.trim().orEmpty()
+
+fun EditText.addTextWatcherSetTextInputLayoutError(
+    til: TextInputLayout,
+    errorTextFunction: (CharSequence) -> CharSequence?
+) = addTextChangedListener(TextWatcherSetTextInputLayoutError(til, errorTextFunction))
+
+private class TextWatcherSetTextInputLayoutError(
+    private val til: TextInputLayout,
+    private val errorTextFunction: (CharSequence) -> CharSequence?
+) : TextWatcherAdapter {
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        til.error = errorTextFunction(s)
+    }
+
+}
 
 @NotSuitableForJava(reason = "Property wrapper for getDrawable() and setImageDrawable().")
 var ImageView.drawableContent: Drawable?
